1f24876daa59c4837dc4b7a4a0598fcbbf53878b
ACCESSSK <125880373+ACCESSSK@users.noreply.github.com>
2023-07-17 16:40:14 +0530

    Add files via upload

---
+++ Assignment1-2.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<string.h>
+
+int main(int argc, char *argv[])
+{
+    int fd = 0;
+    int Mode = 0;
+
+    if (argc !=3)
+    {
+        printf("Inavalid number of arguments\n");
+        return -1;
+    }
+
+    if (strcmp(argv[2],"Read") == 0)
+    {
+        Mode =O_RDONLY;
+    }
+    else if (strcmp(argv[2],"write") == 0)
+    {
+        Mode = O_WRONLY;
+    }
+    else
+    {
+        Mode = O_RDONLY;
+    }
+    
+    
+    fd = open(argv[1], Mode);
+    if (fd == -1)
+    {
+        printf("Unable to open file\n");
+        return -1;
+    }
+    else
+    {
+        printf("File is succesfully opened with fd : %d\n",fd);
+
+    }
+
+    return 0;
+}
